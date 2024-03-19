2da66cdd30b2242129bfbf001781d8f019fdf1e1
nihar <sahooniharranjan41@gmail.com>
2024-03-19 17:47:57 +0530

    Model pusher, app creation completed

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
+# # from us_visa.logger import logging
+
+# # logging.info("Welcome to my custom log")
+
 # from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
 
-# logging.info("Welcome to my custom log")
+# try:
+#     a = 1 / "10" 
+# except Exception as e:
+#     raise USvisaException(e, sys) from e
 
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-try:
-    a = 1 / "10" 
-except Exception as e:
-    raise USvisaException(e, sys) from e
+
+pipline  = TrainPipeline()
+pipline.run_pipeline()
